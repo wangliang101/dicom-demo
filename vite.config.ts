c411e39a2178541186c6b401dbedb4208aee0787
wliang <wliang@infervision.com>
2023-03-20 13:24:04 +0800

    deploy: github page test

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { createStyleImportPlugin, AntdResolve } from "vite-plugin-style-import";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createStyleImportPlugin, AntdResolve } from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,4 +18,5 @@ export default defineConfig({
       },
     },
   },
+  base: 'dicom-demo',
 });
