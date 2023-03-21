63fb5f49db65089f7883ac63ac55c5d5c5222190
wliang <wliang@infervision.com>
2023-03-21 20:55:27 +0800

    fix: 修改github 部署配置

--- deploy.sh
+++ deploy.sh
@@ -1,51 +1,26 @@
-# Simple workflow for deploying static content to GitHub Pages
-name: Deploy static content to Pages
+#!/usr/bin/env sh
 
-on:
-  # 仅在推送到默认分支时运行。
-  push:
-    branches: ['main']
+# 发生错误时终止
+set -e
+# 删除dist, 否则第二次会提示fatal: A branch named 'main' already exists.
+rm -rf dist
 
-  # 这个选项可以使你手动在 Action tab 页面触发工作流
-  workflow_dispatch:
+# 构建
+npm run build
 
-# 设置 GITHUB_TOKEN 的权限，以允许部署到 GitHub Pages。
-permissions:
-  contents: read
-  pages: write
-  id-token: write
+# 进入构建文件夹
+cd dist
 
-# 允许一个并发的部署
-concurrency:
-  group: 'pages'
-  cancel-in-progress: true
+# 如果你要部署到自定义域名
+# echo 'www.example.com' > CNAME
 
-jobs:
-  # 单次部署的工作描述
-  deploy:
-    environment:
-      name: github-pages
-      url: ${{ steps.deployment.outputs.page_url }}
-    runs-on: ubuntu-latest
-    steps:
-      - name: Checkout
-        uses: actions/checkout@v3
-      - name: Set up Node
-        uses: actions/setup-node@v3
-        with:
-          node-version: 18
-          cache: 'npm'
-      - name: Install dependencies
-        run: npm install
-      - name: Build
-        run: npm run build
-      - name: Setup Pages
-        uses: actions/configure-pages@v3
-      - name: Upload artifact
-        uses: actions/upload-pages-artifact@v1
-        with:
-          # Upload dist repository
-          path: './dist'
-      - name: Deploy to GitHub Pages
-        id: deployment
-        uses: actions/deploy-pages@v1
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+
+# 如果你要部署在 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:wangliang101/dicom-demo.git main:gh-pages
+
+cd -
